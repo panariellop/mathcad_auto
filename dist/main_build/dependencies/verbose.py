6529b0c275d46edc5579bb4048eac4122ec1f872
panariellop <53383249+panariellop@users.noreply.github.com>
2021-08-02 13:19:11 -0400

    validation and verbosity

---
+++ dist/main_build/dependencies/verbose.py
@@ -0,0 +1,14 @@
+"""
+Gives verbose version of the argument 
+"""
+
+def verbose_input(cur_input):
+    conversion = {
+        "w_p_input": "WP",
+        "s_ds_input": "S_DS",
+        "a_p_input": "a_P"
+
+    }
+
+if __name__ == "__main__":
+    pass
